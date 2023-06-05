fa9fb4ff3438ec5cf7f999f585345f5fa3dd4300
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-05 15:41:40 -0600

    Proyecto.js Done

---
+++ src/components/Proyecto.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { trabajos } from '../data/trabajos';
+
+export const Proyecto = () => {
+
+    const [proyecto, setProyecto] = useState({});
+    const params = useParams();
+
+    useEffect(() => {
+        let proyecto = trabajos.filter(trabajo => trabajo.id === params.id);
+        setProyecto(proyecto[0]);
+    }, []);
+
+  return (
+    <div className='page page-work'>
+        <div className='mask'>
+          <img src={'/images/'+proyecto.id+'.png'} alt={proyecto.nombre}/>
+        </div>
+        <h1 className='heading'>{proyecto.nombre}</h1>
+        <p>{proyecto.tecnologías}</p>
+        <p>{proyecto.descripción}</p>
+        <a href={'https://'+proyecto.url} target='_blank'>Ir al Proyecto</a>
+
+    </div>
+  )
+}
--- src/router/Rutas.js
+++ src/router/Rutas.js
@@ -7,6 +7,7 @@ import { Curriculum } from '../components/Curriculum';
 import { Contacto } from '../components/Contacto';
 import { HeaderNav } from '../components/layout/HeaderNav';
 import { Footer } from '../components/layout/Footer';
+import { Proyecto } from '../components/Proyecto';
 
 export const Rutas = () => {
   return (
@@ -23,6 +24,7 @@ export const Rutas = () => {
                 <Route path="/servicios" element={<Servicios />}/>
                 <Route path="/curriculum" element={<Curriculum />}/>
                 <Route path="/contacto" element={<Contacto />}/>
+                <Route path="/proyecto/:id" element={<Proyecto />}/>
                 <Route path="*" element={
                     <div className='page'>
                         <h1 className='heading'>Error 404</h1>
